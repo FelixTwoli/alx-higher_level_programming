750d5b04c85db25334f7570bba9ae37e36087310
Felix12tw <felixtwoli21@gmail.com>
2021-05-12 11:13:13 +0000

    program that prints all names defined by hidden module 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if _name_ == "_main_";
+	from hidden_4 import *
+	arr = dir()
+	for i in range(0, len(arr)):
+		        if arr[i][0:2] != "__":
+			            print("{}".format(arr[i]))
