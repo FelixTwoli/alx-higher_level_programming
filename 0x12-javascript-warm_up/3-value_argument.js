204e99ba8f6978f64c36d24879b551c424c352dd
FelixTwoli <felixtwoli21@gmail.com>
2021-08-03 12:39:01 +0000

    removed some spaces

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,19 +1,9 @@
 #!/usr/bin/node
-
 const process = require('process');
-
-
-
 const args = process.argv;
-
-
-
 if (args[2]) {
-
-      console.log(args[2]);
-
-    } else {
-
-	  console.log('No argument');
-
-	}
+    console.log(args[2]);
+      }
+       else {
+	   console.log('No argument');
+       }
