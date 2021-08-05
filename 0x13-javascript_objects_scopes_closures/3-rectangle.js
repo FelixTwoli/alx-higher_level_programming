6a787579903de9d2721ccf715b50bcf34ffe72c2
FelixTwoli <felixtwoli21@gmail.com>
2021-08-05 03:31:07 +0000

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+//Describes rectangle class definition
+
+class Rectangle {
+    constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let p = 'X';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width - 1; j++) {
+        p = p + 'X';
+      }
+      console.log(p);
+      p = 'X';
+    }
+  }
+}
+
+module.exports = Rectangle;
